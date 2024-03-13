1fb481061b57822e84a4610415aa5ce8ebc01074
andy <yagosys@gmail.com>
2024-03-13 10:34:15 +0000

    add delete channel if already exist

--- hyperledger/3_install_hyperledge.sh
+++ hyperledger/3_install_hyperledge.sh
@@ -2,6 +2,7 @@
 cd $GOPATH
 ./install-fabric.sh docker samples binary
 cd fabric-samples/test-network
+./network.sh down
 ./network.sh up createChannel -ca -c mychannel -s couchdb
 ./network.sh deployCC -ccn basic -ccp ../asset-transfer-basic/chaincode-go/ -ccl go
 #docker means pull docker image
