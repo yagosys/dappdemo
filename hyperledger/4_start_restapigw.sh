bea38bcd020185d0bcc4bf961b094b7f222befd9
andy <yagosys@gmail.com>
2024-03-13 03:37:25 +0000

    update

--- hyperledger/4_start_restapigw.sh
+++ hyperledger/4_start_restapigw.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -xe 
-cd $GOPATH/fabric-samples/asset-transfer-basic/rest-api-go
+echo kill existing rest-api
+  pids=$(pgrep -f "go run main.go")
+    for pid in $pids; do
+        echo "Killing PID $pid and its child processes."
+
+        # Attempt to kill child processes
+        pkill -P $pid
+
+        # Kill the parent process
+        kill $pid
+    done
+   
+echo start rest-api
+
+cd rest-api-go
 go mod download
 go run main.go &
